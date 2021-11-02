1a7332fff4e11d55da7c0e72979f63d728118659
FrancisOhemengA <93487638+fadu2@users.noreply.github.com>
2021-11-02 19:54:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,89 @@
+class Main {
+   public static void main(String[] args) {
+  // //   System.out.println("   /_\\\n  /___\\\n /_____\\\n   /_\\");
+
+  // // System.out.println("/\\_\\_\\_\\\n|      |\n|______|");
+
+  // // System.out.println("He said \"Run fast\"");
+
+  // // Math operators
+  // // + - * / % (modulus)
+
+  // // int a = 9;
+  // // int b = 5;
+
+  // // int c = a + b;
+  // // System.out.println(c);  // 9 + 5 = 14
+
+  // // int d = a - b;
+  // // System.out.println(d); // 9 - 5 = 4
+
+  // // int e = a * b;
+  // // System.out.println(e); // 9 * 5 = 45
+
+  // // int f = a / b;
+  // // System.out.println(f); // 9 / 5 = 1
+  
+  // // System.out.println(5 / 2);  //2
+  // // System.out.println(5.0/2);  // 2.5
+  // // System.out.println(5 / 2.0);  // 2.5
+  // // System.out.println(1.0 * a / b); // 1.8
+
+  // // PEMDAS 
+  // // % Modulus
+  // System.out.println(3 % 2);  // 1
+  // System.out.println(7 % 3); // 1
+  // System.out.println(8 % 3);  // 2
+
+  // System.out.println(7 % 8);
+  // System.out.println(100 % 101);
+  // System.out.println(101 % 100);
+  // 1 % 2 = 1
+  // 2 % 2 = 0
+  // 3 % 2 = 1
+  // 4 % 2 = 0
+
+  // 3 - 6 % 2 = 3
+  // 3 + 4 / 3 % 2 = 4
+
+  // Unary operators 
+  // ++. -- 
+  int a = 4;
+  a++;  // a = a + 1;
+  System.out.println(a);
+
+  int b = 7;
+  b--;  // b = b -1;
+  System.out.println(b);
+
+  int c = 8;
+  // += , -=, *= /=, %=
+  c += 2;  // c = c + 2;
+  System.out.println(c);
+
+  c -= 3;  // c = c - 3;
+  System.out.println(c);
+
+  c *= 2;  // c = c * 2;
+  System.out.println(c);
+
+  c /= 7;  //  c = c / 7;
+  System.out.println(c);
+
+  c %= 3;  // c = c % 3;
+  System.out.println(c);
+
+  int n = 10;
+  // n % 5 * 7 = 0
+
+  // n / 2 % 3 = 2
+
+  // 1 % 2 = 1
+  // 3 % 4 = 3
+  
+
+
+
+
+  }
+}
